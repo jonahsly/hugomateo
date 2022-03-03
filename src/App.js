6a2b85e16e632ae18a036870550f23f55d74ca42
jonahsly <slyjonatan@gmail.com>
2022-03-03 00:28:55 -0300

    aditions_v2

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
       /** Versión alfa base **/
 
   return (
-    <div className="App">
+    /**<div className="App">
       <header className="App-header" id='head'>
         <nav className="App-header-nav">
           <img className="App-header-nav-logo" src={logo} alt="logo" />
@@ -46,6 +46,21 @@ function App() {
         <div className="slide" id="slide1"><h1>Slide Backgrounds</h1></div>
         <div className="slide" id="slide2"><h1>Totally customizable</h1></div>
       </div>
+      <div id="fullpage">
+	      <div class="section" id="section1">
+		      <div class="slide" id="slide1"><h1>Foto 1</h1></div>
+		      <div class="slide" id="slide2"><h1>Foto 2</h1></div>
+	      </div>
+      </div>
+    </div>**/
+    <div id="fullpage">
+      <div class="section" id="section1">
+        <div class="slide" id="slide1"><h1>Foto 1</h1></div>
+        <div class="slide" id="slide2"><h1>Foto 2</h1></div>
+      </div>
+      <div class="section" id="section0"><h1>Hugo Mateo Bertone</h1></div>
+      <div class="section" id="section2"><h1>Foto 3<br />0000</h1></div>
+      <div class="section" id="section3"><h1>Foto 4</h1></div>
     </div>
   );
 }
